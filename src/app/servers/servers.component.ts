c20e1d50c3399a32d173208f6c80b6ce5734f35d
Riadh BEL HADJ SALAH <riadh_bel_hadj2004@hotmail.com>
2019-04-21 18:06:43 +0300

    databinding - property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,12 +7,15 @@ import { Component, OnInit } from '@angular/core';
 //  selector: '[app-servers]',
 // CSS selector    
   selector: '.app-servers',    
-  template: `
-        <app-server></app-server>
-        <app-server></app-server>`,
+//  template: `
+//        <app-server></app-server>
+//        <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+    
+  allowNewServer = false;
 
   constructor() { }
 
