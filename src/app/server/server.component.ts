658dc6ee071fa2b3f34931cfb8a85654cfe79a60
Riadh BEL HADJ SALAH <riadh_bel_hadj2004@hotmail.com>
2019-04-24 12:42:06 +0300

    ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls:  ['./server.component.css']
 })
 
 export class ServerComponent {
     serverId : number = 10;
     serverStatus: string = 'offline'
+    red = "{color : 'red'}"
+    
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
     
     getServerStatus(){
         return this.serverStatus;   
+    }
+    
+    getColor(){
+       return this.serverStatus === 'online' ? 'green' : 'red' ; 
+//       return "blue" ; 
     }
     
 }
