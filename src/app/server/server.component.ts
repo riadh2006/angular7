2f71426ba0edd1ccac85dba7514a7ef202a1b274
Riadh BEL HADJ SALAH <riadh_bel_hadj2004@hotmail.com>
2019-04-24 14:58:31 +0300

    ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls:  ['./server.component.css']
+  styles:  [`
+.online {
+    color: white;    
+}
+
+
+`]
 })
 
 export class ServerComponent {
